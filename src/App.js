3ba4eefa108273299c673e72b21298aceaeb9bb2
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-31 19:56:24 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { ethers, utils } from "ethers";
 import abi from "./contracts/MemeCoin.json";
 
 function App() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
-  const [inputValue, setInputValue] = useState({ walletAddress: "", transferAmount: "", burnAmount: "", mintAmount: "" });
+  const [inputValue, setInputValue] = useState({
+    walletAddress: "",
+    transferAmount: "",
+    burnAmount: "",
+    mintAmount: "",
+  });
   const [tokenName, setTokenName] = useState("");
   const [tokenSymbol, setTokenSymbol] = useState("");
   const [tokenTotalSupply, setTokenTotalSupply] = useState(0);
@@ -13,13 +18,19 @@ function App() {
   const [yourWalletAddress, setYourWalletAddress] = useState(null);
   const [error, setError] = useState(null);
 
-  const contractAddress = 'YOUR CONTRACT ADDRESS HERE';
+  const contractAddress = "YOUR CONTRACT ADDRESS HERE";
   const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
       if (window.ethereum) {
-
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = accounts[0];
+        setIsWalletConnected(true);
+        setYourWalletAddress(account);
+        console.log("Account connected: ", account);
       } else {
         setError("Install a MetaMask wallet to get our token.");
         console.log("No Metamask detected");
@@ -27,22 +38,55 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const getTokenInfo = async () => {
     try {
       if (window.ethereum) {
+        // A provider lets us connect to the ethereum blockchain, in this case the Rinkeby testnet via an ethereum node.
+        // We're using MetaMask's provider which uses Infura behind the scenes.
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Now with our provider we get a signer which is an abstraction of your MetaMask wallet that lets you interact with the blockchain without revealing your private keys.
+        // As a signer, you can write to the ethereum blockchain via transactions
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        const [account] = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        let tokenName = await tokenContract.name();
+        let tokenSymbol = await tokenContract.symbol();
+        let tokenOwner = await tokenContract.owner();
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply);
+
+        setTokenName(`${tokenName}`);
+        setTokenSymbol(tokenSymbol);
+        setTokenTotalSupply(tokenSupply);
+        setTokenOwnerAddress(tokenOwner);
+
+        if (account.toLowerCase() === tokenOwner.toLowerCase()) {
+          setIsTokenOwner(true);
+        }
+
+        console.log("Token Name: ", tokenName);
+        console.log("Token Symbol: ", tokenSymbol);
+        console.log("Token Supply: ", tokenSupply);
+        console.log("Token Owner: ", tokenOwner);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const transferToken = async (event) => {
     event.preventDefault();
     try {
       if (window.ethereum) {
-
       } else {
         console.log("Ethereum object not found, install Metamask.");
         setError("Install a MetaMask wallet to get our token.");
@@ -50,13 +94,12 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const burnTokens = async (event) => {
     event.preventDefault();
     try {
       if (window.ethereum) {
-
       } else {
         console.log("Ethereum object not found, install Metamask.");
         setError("Install a MetaMask wallet to get our token.");
@@ -64,13 +107,12 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const mintTokens = async (event) => {
     event.preventDefault();
     try {
       if (window.ethereum) {
-
       } else {
         console.log("Ethereum object not found, install Metamask.");
         setError("Install a MetaMask wallet to get our token.");
@@ -78,29 +120,44 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleInputChange = (event) => {
-    setInputValue(prevFormData => ({ ...prevFormData, [event.target.name]: event.target.value }));
-  }
+    setInputValue((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
     getTokenInfo();
-  }, [])
+  }, []);
 
   return (
     <main className="main-container">
       <h2 className="headline">
         <span className="headline-gradient">Meme Coin Project</span>
-        <img className="inline p-3 ml-2" src="https://i.imgur.com/5JfHKHU.png" alt="Meme Coin" width="60" height="30" />
+        <img
+          className="inline p-3 ml-2"
+          src="https://i.imgur.com/5JfHKHU.png"
+          alt="Meme Coin"
+          width="60"
+          height="30"
+        />
       </h2>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
         <div className="mt-5">
-          <span className="mr-5"><strong>Coin:</strong> {tokenName} </span>
-          <span className="mr-5"><strong>Ticker:</strong>  {tokenSymbol} </span>
-          <span className="mr-5"><strong>Total Supply:</strong>  {tokenTotalSupply}</span>
+          <span className="mr-5">
+            <strong>Coin:</strong> {tokenName}{" "}
+          </span>
+          <span className="mr-5">
+            <strong>Ticker:</strong> {tokenSymbol}{" "}
+          </span>
+          <span className="mr-5">
+            <strong>Total Supply:</strong> {tokenTotalSupply}
+          </span>
         </div>
         <div className="mt-7 mb-9">
           <form className="form-style">
@@ -120,9 +177,9 @@ function App() {
               placeholder={`0.0000 ${tokenSymbol}`}
               value={inputValue.transferAmount}
             />
-            <button
-              className="btn-purple"
-              onClick={transferToken}>Transfer Tokens</button>
+            <button className="btn-purple" onClick={transferToken}>
+              Transfer Tokens
+            </button>
           </form>
         </div>
         {isTokenOwner && (
@@ -137,9 +194,7 @@ function App() {
                   placeholder={`0.0000 ${tokenSymbol}`}
                   value={inputValue.burnAmount}
                 />
-                <button
-                  className="btn-purple"
-                  onClick={burnTokens}>
+                <button className="btn-purple" onClick={burnTokens}>
                   Burn Tokens
                 </button>
               </form>
@@ -154,9 +209,7 @@ function App() {
                   placeholder={`0.0000 ${tokenSymbol}`}
                   value={inputValue.mintAmount}
                 />
-                <button
-                  className="btn-purple"
-                  onClick={mintTokens}>
+                <button className="btn-purple" onClick={mintTokens}>
                   Mint Tokens
                 </button>
               </form>
@@ -164,18 +217,28 @@ function App() {
           </section>
         )}
         <div className="mt-5">
-          <p><span className="font-bold">Contract Address: </span>{contractAddress}</p>
+          <p>
+            <span className="font-bold">Contract Address: </span>
+            {contractAddress}
+          </p>
         </div>
         <div className="mt-5">
-          <p><span className="font-bold">Token Owner Address: </span>{tokenOwnerAddress}</p>
+          <p>
+            <span className="font-bold">Token Owner Address: </span>
+            {tokenOwnerAddress}
+          </p>
         </div>
         <div className="mt-5">
-          {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{yourWalletAddress}</p>}
+          {isWalletConnected && (
+            <p>
+              <span className="font-bold">Your Wallet Address: </span>
+              {yourWalletAddress}
+            </p>
+          )}
           <button className="btn-connect" onClick={checkIfWalletIsConnected}>
             {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
           </button>
         </div>
-
       </section>
     </main>
   );
